561b7d659469d161a836b262ca162cbbc249b041
Mohith-96-Reddy <reddymohith9966@gmail.com>
2023-03-18 11:25:32 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        // obj.name = "John"; // this will give an error as name is private
+
+        // Uncomment the below line to see the error
+        // System.out.println(obj.name);
+
+        obj.setName("John");
+        System.out.println(obj.getName()); // This will print "John"
+    }
 }
